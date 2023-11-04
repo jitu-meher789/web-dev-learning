83d4de14207c57f559d9975361785bae3e025c5b
Jitu Meher <jitumeher217@gmail.com>
2023-11-04 21:29:45 +0530

    camera completed

---
+++ Others/Lect - 16 camera - 5/dbScript.js
@@ -0,0 +1,106 @@
+let req = indexedDB.open("Camera", 1);
+let db;
+let gbody = document.querySelector("body");
+
+req.addEventListener("success", function () {
+  db = req.result;
+});
+
+// upgrade event occurs in 2 cases
+// case1 : when we create a new db
+// case2 : when we try to open a database which already existed with a incremented version
+req.addEventListener("upgradeneeded", function () {
+  let accessToCameraDB = req.result;
+  accessToCameraDB.createObjectStore("Gallery", { keyPath: "mId" });
+});
+
+req.addEventListener("error", function () {
+  alert("error in creation");
+});
+
+function addMedia(media, type) {
+  if (!db) return;
+  let obj = { mId: Date.now(), media, type };
+
+  let tx = db.transaction("Gallery", "readwrite");
+
+  let gallery = tx.objectStore("Gallery");
+
+  gallery.add(obj);
+}
+
+function viewMedia() {
+  if (!db) return;
+  let tx = db.transaction("Gallery", "readonly");
+
+  let gallery = tx.objectStore("Gallery");
+
+  let cReq = gallery.openCursor();
+
+  cReq.addEventListener("success", function () {
+    let cursor = cReq.result;
+    if (cursor) {
+      //   let li = document.createElement("li");
+      //   li.innerHTML = `<div>${cursor.value.title}</div><div>${cursor.value.content}</div> <br>`;
+      //   ul.append(li);
+
+      let mo = cursor.value;
+      let linkForDownloadBtn = "";
+      let div = document.createElement("div");
+      div.classList.add("media-container");
+
+      if (mo.type == "video") {
+        let url = window.URL.createObjectURL(cursor.value.media);
+        linkForDownloadBtn = url;
+        div.innerHTML = `<div class="media">
+                            <video src="${url}" autoplay loop controls muted></video>
+                        </div>
+                        <button class="download">Download</button>
+                        <button class="delete" data-id="${mo.mId}"   >Delete</button>`;
+        gbody.append(div);
+      } else {
+        linkForDownloadBtn = mo.media;
+        div.innerHTML = `<div class="media">
+                            <a href="${mo.media}">
+                                <img src="${mo.media}">
+                            </a>
+                        </div>
+                        <button class="download">Download</button>
+                        <button class="delete" data-id="${mo.mId}">Delete</button>`;
+      }
+
+      let downloadBtn = div.querySelector(".download");
+      let deleteBtn = div.querySelector(".delete");
+      downloadBtn.addEventListener("click", function (e) {
+        let a = document.createElement("a");
+        a.href = linkForDownloadBtn;
+
+        if (mo.type == "video") {
+          a.download = "video.mp4";
+        } else {
+          a.download = "img.png";
+        }
+        a.click();
+        a.remove();
+      });
+
+      deleteBtn.addEventListener("click", function (e) {
+        let id = e.currentTarget.getAttribute("data-id");
+        // remove from db
+        deleteMedia(id);
+        // remove from ui
+        e.currentTarget.parentElement.remove();
+      });
+
+      gbody.append(div);
+      cursor.continue();
+    }
+  });
+}
+
+function deleteMedia(id) {
+  if (!db) return;
+  let tx = db.transaction("Gallery", "readwrite");
+  let gallery = tx.objectStore("Gallery");
+  gallery.delete(Number(id));
+}
---
+++ Others/Lect - 16 camera - 5/main.js
@@ -0,0 +1,168 @@
+let video = document.querySelector("video");
+let recordBtn = document.querySelector("#record");
+let recDiv = recordBtn.querySelector("div");
+let captureBtn = document.querySelector("#capture");
+let capDiv = captureBtn.querySelector("div");
+let body = document.querySelector("body");
+
+let isRecording = false;
+let mediaRecorder;
+let chunks = [];
+
+let minZoom = 1;
+let maxZoom = 3;
+let zoomInBtn = document.querySelector(".zoom-in");
+let zoomOutBtn = document.querySelector(".zoom-out");
+let currZoom = 1;
+let appliedFilter;
+let filters = document.querySelectorAll(".filter");
+
+let galleryBtn = document.querySelector("#gallery");
+
+galleryBtn.addEventListener("click",function(e){
+    location.assign("gallery.html");
+    showMedia();
+});
+
+
+
+
+zoomInBtn.addEventListener("click",function() {
+    if(currZoom < maxZoom){
+        currZoom = currZoom + 0.1;
+
+    }
+    video.style.transform = `scale(${currZoom})`;
+});
+
+
+
+zoomOutBtn.addEventListener("click",function() {
+    if(currZoom > minZoom) {
+        currZoom = currZoom - 0.1;
+    }
+    video.style.transform = `scale(${currZoom})`;
+});
+
+
+
+
+
+for(let i = 0; i < filters.length; i++) {
+    filters[i].addEventListener("click", function(e){
+        console.log(e.currentTarget.style.backgroundColor);
+        removeFilter();
+        appliedFilter = e.currentTarget.style.backgroundColor;
+        let div = document.createElement("div");
+        div.style.backgroundColor = appliedFilter;
+        div.classList.add("filter-div");
+        body.append(div);
+    })
+}
+
+// startBtn.addEventListener("click", () => {
+//     mediaRecorder.start();
+// });
+// stopBtn.addEventListener("click", () => {
+//     mediaRecorder.stop();
+// });
+
+recordBtn.addEventListener("click", (e) => {
+    if (isRecording) {
+        mediaRecorder.stop();
+        isRecording = false;
+        recDiv.classList.remove("record-animation");
+        alert("Video Recored");
+    } else {
+        mediaRecorder.start();
+        appliedFilter = "";
+        removeFilter();
+        currZoom = 1;
+        video.style.transform = `scale(${currZoom})`;
+        isRecording = true;
+        recDiv.classList.add("record-animation");
+    }
+});
+
+captureBtn.addEventListener("click", function () {
+    let snd = new Audio("captured1.wav");
+    snd.play()
+
+    capDiv.classList.add("capture-animation");
+    
+    setTimeout(function () {
+        capDiv.classList.remove("capture-animation");
+    },1000)
+
+    let canvas = document.createElement("canvas");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    let tool = canvas.getContext("2d");
+
+    tool.translate(canvas.width / 2, canvas.height / 2);
+    tool.scale(currZoom, currZoom);
+    tool.translate(-canvas.width / 2, -canvas.height / 2);
+
+
+    tool.drawImage(video, 0, 0);
+
+    if(appliedFilter) {
+        tool.fillStyle = appliedFilter;
+        tool.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+
+    // for image we will save this data url in the db
+    let link = canvas.toDataURL();
+    addMedia(link, "image");
+
+
+    // let a = document.createElement("a");
+    // a.href = link;
+    // a.download = "img.png";
+    // a.click();
+    // a.remove();
+    // canvas.remove();
+    setTimeout(function () {
+        alert("Captured");
+    },1000)
+    
+    // location.assign("gallery.html");
+})
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(function (mediaStream) {
+
+        mediaRecorder = new MediaRecorder(mediaStream);
+
+        mediaRecorder.addEventListener("dataavailable", (e) => {
+            chunks.push(e.data);
+        })
+
+        mediaRecorder.addEventListener("stop", () => {
+            let blob = new Blob(chunks, { type: "video/mp4" });
+            chunks = [];
+            addMedia(blob, "video");
+
+            // let a = document.createElement("a");
+            // a.href = URL.createObjectURL(blob);
+            // a.download = "jitumeher.mp4";
+            // a.click();
+            // a.remove();
+        })
+
+        video.srcObject = mediaStream;
+
+
+
+    }).catch(function (error) {
+        console.log(error);
+    })
+
+
+    function removeFilter() {
+        let filter = document.querySelector(".filter-div");
+        if(filter) {
+            filter.remove();
+        }
+    }
